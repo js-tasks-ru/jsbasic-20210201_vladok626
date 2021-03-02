fa0bb2fa185d61c9061a3de71282d73b40920f38
Vlad Nurpiisov <vladok626@gmail.com>
2021-03-02 21:24:10 +0500

    решил 5 модуль

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  
+  let leftButton = document.querySelector('.carousel__arrow_left')
+  let rightButton = document.querySelector('.carousel__arrow_right')
+  let slide = document.querySelector('.carousel__inner')
+  let slideOffset = document.querySelector('.carousel__inner').offsetWidth
+  let startPosition = 0
+
+  leftButton.style.display = 'none'
+
+  leftButton.onclick = function () {
+    rightButton.style.display = ''
+    startPosition += slideOffset
+    slide.style.transform = `translateX(${startPosition}px)`
+    if (startPosition == 0) {
+      leftButton.style.display = 'none';
+    }
+  }
+
+  rightButton.onclick = function () {
+    leftButton.style.display = ''
+    startPosition -= slideOffset
+    slide.style.transform = `translateX(${startPosition}px)`
+    if (startPosition < -2 * slideOffset) {
+      rightButton.style.display = 'none';
+    }
+  }
 }
